0db0afcb593790366d6cb85e6aefdddda2621084
zhangxiaodao <1547100913@qq.com>
2017-05-08 15:14:47 +0800

    Initial Commit

---
+++ 温控仪/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  温控仪
+//
+//  Created by 杭州阿尔法特 on 2017/5/8.
+//  Copyright © 2017年 张海昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
